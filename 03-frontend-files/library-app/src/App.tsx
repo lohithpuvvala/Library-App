acd894b49aa3765c4f0fbedcc3b6f86682e66c34
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-03 00:32:34 +0530

    Create Heros.tsx (Hompage->Heros Component). Import at App.tsx

--- 03-frontend-files/library-app/src/App.tsx
+++ 03-frontend-files/library-app/src/App.tsx
@@ -2,13 +2,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/Hompage/ExploreTopBooks';
 import { Carousel } from './layouts/Hompage/Carousel';
+import { Heros } from './layouts/Hompage/Heros';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
     </div>
   );
 }
