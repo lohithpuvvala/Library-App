eef43bfa98e14d2d8c32366ba6835e5b43b0f7f1
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-02 19:36:51 +0530

    Create Carousel.tsx (layout->homepage) for Desktop

--- 03-frontend-files/library-app/src/App.tsx
+++ 03-frontend-files/library-app/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/Hompage/ExploreTopBooks';
+import { Carousel } from './layouts/Hompage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
